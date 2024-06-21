e1d40ba3532dc5fd573d54d27622dcf36f485c75
Pericles <slouis482@gmail.com>
2024-06-21 02:05:27 -0400

    se esta creando el formulario de monedas

--- src/app/modules/configuracion/pages/monedas-crud/monedas-crud/monedas-crud.component.ts
+++ src/app/modules/configuracion/pages/monedas-crud/monedas-crud/monedas-crud.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/components/login/services/Auth.service';
+import { Modules } from 'src/app/enum/Modules';
+import { Access } from 'src/app/models/Access';
+import { ToastService } from 'src/app/services/toast/toast.service';
 
 @Component({
   selector: 'app-monedas-crud',
   templateUrl: './monedas-crud.component.html',
   styleUrls: ['./monedas-crud.component.scss']
 })
-export class MonedasCrudComponent {
+export class MonedasCrudComponent implements OnInit {
 
+  access: Access = new Access();
+  loading: boolean = false
+  constructor(
+    private authService: AuthService,
+    private toastService: ToastService
+  ) { }
+  ngOnInit(): void {
+    this.accessModule()
+  }
+  /**
+   *Obtiene los permisos del usuario
+   *
+   * @memberof MonedasCrudComponent
+   */
+  accessModule() {
+    this.loading = true
+    this.authService.accessModule(Modules.monedas).subscribe({
+      next: (access) => {
+        this.access = access
+        this.loading = false
+      }, error: (err) => {
+        this.loading = false
+        this.toastService.error('Error en permisos')
+      },
+    })
+  }
 }
