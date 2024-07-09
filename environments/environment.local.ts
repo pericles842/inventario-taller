6e54fa46844bbf450cfa047b5654e1d8def5893f
PeriClesII <60017068+pericles842@users.noreply.github.com>
2024-07-09 02:50:39 -0400

    Update environment.local.ts
    edicion de env

--- environments/environment.local.ts
+++ environments/environment.local.ts
@@ -1,6 +1,6 @@
 import { configuration } from "./TypeEnvironments";
 
 export const environments: { host: string, configuration: configuration } = {
-    host: "http://54.162.166.247:8000/",
+    host: "http://54.162.166.247/",
     configuration: 'local'
 }
